9ffb9ee5a675ed56bedb5b1293de7dc6bc1d81b1
nitheeshkp24@gmail.com <nitheeshkp24@gmail.com>
2020-11-15 19:59:34 +0530

    class

---
+++ Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Newclass ob = new Newclass();
+            string a = "Hello";
+            string b = "World";
+
+            ob.Display(a, b);
+        }
+        
+    }
+}
+class Newclass
+{
+    public int MyProperty { get; set; }
+    public void Display(string a,string b)
+    {
+        Console.WriteLine(a+" "+b);
+    }
+}
