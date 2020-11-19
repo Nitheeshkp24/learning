bbc813a316168c9e4663426376029bc219e36db6
nitheeshkp24@gmail.com <nitheeshkp24@gmail.com>
2020-11-19 20:44:26 +0530

    .

---
+++ Value Type and Reference Type/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Value_Type_and_Reference_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+                int i = 100;
+
+                Console.WriteLine(i);
+
+                ChangeValue(i);
+
+                Console.WriteLine(i);
+
+            Student std1 = new Student();
+            std1.StudentName = "Bill";
+
+            ChangeReferenceType(std1);
+
+            Console.WriteLine(std1.StudentName);
+
+        }
+        static void ChangeValue(int x)
+        {
+            x = 200;
+
+            Console.WriteLine(x);
+        }
+        static void ChangeReferenceType(Student std2)
+        {
+            std2.StudentName = "Steve";
+        }
+
+    }
+}
---
+++ Value Type and Reference Type/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Value_Type_and_Reference_Type
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+    }
+}
